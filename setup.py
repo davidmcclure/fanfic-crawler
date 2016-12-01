a53506de7f0419eed886ef3dc3d79504d3ed0ce3
David McClure <davidwilliammcclure@gmail.com>
2016-12-01 14:24:33 -0800

    Alembic scaffolding.

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+
+    name='fanfic-crawler',
+    version='0.1.0',
+    description='Harry Potter fan fiction.',
+    url='https://github.com/davidmcclure/fanfic-crawler',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+
+)
