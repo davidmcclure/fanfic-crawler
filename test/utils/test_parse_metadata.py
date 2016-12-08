e7f95d8c760c47beeea3df3fd6fe2750c464a5c8
David McClure <davidwilliammcclure@gmail.com>
2016-12-08 13:35:30 -0800

    More test cases for parse_metadata().

--- test/utils/test_parse_metadata.py
+++ test/utils/test_parse_metadata.py
@@ -30,6 +30,47 @@ from fanfic.utils import parse_metadata
 
     ),
 
+    (
+
+        '''
+        Rated: Fiction K - English - Romance - Hermione G., Ron W. - Chapters:
+        20 - Words: 67,939 - Reviews: 608 - Favs: 62 - Follows: 13 - Updated:
+        Mar 30, 2001 - Published: Jan 2, 2001 - id: 162160
+        ''',
+
+        dict(
+            rating='Fiction K',
+            language='English',
+            genres='Romance',
+            characters='Hermione G., Ron W.',
+            favorites=62,
+            follows=13,
+            published=date(2001, 1, 2),
+        )
+
+    ),
+
+    (
+
+        '''
+        Rated: Fiction T - English - Angst/Romance - Hermione G., Draco M.,
+        Lucius M., Narcissa M. - Chapters: 12 - Words: 69,397 - Reviews: 12 -
+        Favs: 19 - Follows: 34 - Updated: Dec 4 - Published: Nov 20 - id:
+        12241223
+        ''',
+
+        dict(
+            rating='Fiction T',
+            language='English',
+            genres='Angst/Romance',
+            characters='Hermione G., Draco M., Lucius M., Narcissa M.',
+            favorites=19,
+            follows=34,
+            published=date(2016, 11, 20),
+        )
+
+    ),
+
 ])
 def test_parse_metadata(raw, parsed):
     assert parse_metadata(raw) == parsed
