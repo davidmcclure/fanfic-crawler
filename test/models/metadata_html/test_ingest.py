66b7c52b3a79239601ae8e9628b210968535d322
David McClure <davidwilliammcclure@gmail.com>
2016-12-09 15:39:06 -0800

    Full integration test for MetadataHTML.ingest().

---
+++ test/models/metadata_html/test_ingest.py
@@ -0,0 +1,36 @@
+
+
+import pytest
+
+from fanfic.database import session
+from fanfic.models import MetadataHTML, Metadata
+from fanfic.utils import read_yaml
+
+
+pytestmark = pytest.mark.usefixtures('db')
+
+
+cases = read_yaml(__file__, 'fixtures.yml')
+
+
+@pytest.fixture(scope='module', autouse=True)
+def ingest():
+
+    """
+    Write HTML fixtures into the database.
+    """
+
+    for book_id, case in cases.items():
+        row = MetadataHTML(book_id=book_id, html=case['html'])
+        session.add(row)
+
+    MetadataHTML.ingest()
+
+
+@pytest.mark.parametrize('book_id,case', cases.items())
+def test_ingest(book_id, case):
+
+    row = Metadata.query.filter_by(book_id=book_id).one()
+
+    for key, val in case['fields'].items():
+        assert getattr(row, key) == val
