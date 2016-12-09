ba8aa0a2e24401b31e45e4f8afb210823d47ee93
David McClure <davidwilliammcclure@gmail.com>
2016-12-09 11:10:58 -0800

    Two metadata parse cases.

--- test/models/metadata_html/test_parse.py
+++ test/models/metadata_html/test_parse.py
@@ -6,34 +6,61 @@ from fanfic.models import MetadataHTML
 
 
 @pytest.mark.parametrize('html,fields', [
+
     (
 
         '''
-<div id="profile_top" style="min-height:112px;"><button class="btn pull-right icon-heart" type="button" onclick='$("#follow_area").modal();'> Follow/Fav</button><b class="xcontrast_txt">The Conservation of Fame</b>
-<span class="xcontrast_txt"><div style="height:5px"></div>By:</span> <a class="xcontrast_txt" href="/u/1265079/Lomonaaeren">Lomonaaeren</a> <span class="icon-mail-1  xcontrast_txt"></span> <a class="xcontrast_txt" title="Send Private Message" href="https://www.fanfiction.net/pm2/post.php?uid=1265079"></a>
-<div style="margin-top:2px" class="xcontrast_txt">HPDM slash. Harry has peace at last under a spell that keeps people from remembering he's the Chosen One, until Malfoy stumbles through his wards, pursued by mysterious enemies. Harry has to help him, and possibly bring his peace crashing down. COMPLETE.</div>
-<span class="xgray xcontrast_txt">Rated: <a class="xcontrast_txt" href="https://www.fictionratings.com/" target="rating">Fiction  M</a> - English - Romance/Drama -  Draco M., Harry P. - Chapters: 19   - Words: 60,015 - Reviews: <a href="/r/7926634/">611</a> - Favs: 904 - Follows: 498 - Updated: <span data-xutime="1481087877">12/6</span> - Published: <span data-xutime="1331829125">3/15/2012</span> - Status: Complete - id: 7926634 </span>
+<div id="profile_top" style="min-height:112px;"><span style="cursor:pointer;" title="Click for Larger Image" onclick="var t = $('#img_large img');t.prop('src',t.attr('data-original'));$('#img_large').modal();"><img class="cimage " style="clear:left;float:left;margin-right:3px;padding:2px;border:1px solid #ccc;-moz-border-radius:2px;-webkit-border-radius:2px;" src="//ffcdn2012t-fictionpressllc.netdna-ssl.com/image/3636437/75/" width="75" height="100"></span><button class="btn pull-right icon-heart" type="button" onclick='$("#follow_area").modal();'> Follow/Fav</button><b class="xcontrast_txt">Whispers in the Dark</b>
+<span class="xcontrast_txt"><div style="height:5px"></div>By:</span> <a class="xcontrast_txt" href="/u/5880664/IronSparrow99">IronSparrow99</a> <span class="icon-mail-1  xcontrast_txt"></span> <a class="xcontrast_txt" title="Send Private Message" href="https://www.fanfiction.net/pm2/post.php?uid=5880664"></a>
+<div style="margin-top:2px" class="xcontrast_txt">There are certain things that are taken for fact in the magical world: the dead cannot return, Dumbledore know and sees all, and, before now, once you've lived thirteen years as one person, you're that person until you die. As I am now finding out, this is not true. And now...I might just be screwed. (AU PoA) *Underwent a title change*</div>
+<span class="xgray xcontrast_txt">Rated: <a class="xcontrast_txt" href="https://www.fictionratings.com/" target="rating">Fiction  T</a> - English - Family/Adventure -  Harry P., Ron W., Hermione G., OC - Chapters: 45   - Words: 99,425 - Reviews: <a href="/r/11472121/">89</a> - Favs: 58 - Follows: 84 - Updated: <span data-xutime="1481309053">18m</span> - Published: <span data-xutime="1440631530">8/26/2015</span> - id: 11472121 </span>
 </div>
         ''',
 
         dict(
-            title='The Conservation of Fame',
-            user_id=1265079,
-            username='Lomonaaeren',
-            summary='HPDM slash. Harry has peace at last under a spell that keeps people from remembering he\'s the Chosen One, until Malfoy stumbles through his wards, pursued by mysterious enemies. Harry has to help him, and possibly bring his peace crashing down. COMPLETE.',
-            follows=498,
-            favorites=904,
-            rating='Fiction M',
+            title='Whispers in the Dark',
+            user_id=5880664,
+            username='IronSparrow99',
+            summary='There are certain things that are taken for fact in the magical world: the dead cannot return, Dumbledore know and sees all, and, before now, once you\'ve lived thirteen years as one person, you\'re that person until you die. As I am now finding out, this is not true. And now...I might just be screwed. (AU PoA) *Underwent a title change*',
+            follows=84,
+            favorites=58,
+            rating='Fiction T',
             language='English',
-            genres='Romance/Drama',
-            characters='Draco M., Harry P.',
+            genres='Family/Adventure',
+            characters='Harry P., Ron W., Hermione G., OC',
         )
 
     ),
+
+    (
+
+        '''
+<div id="profile_top" style="min-height:112px;"><span style="cursor:pointer;" title="Click for Larger Image" onclick="var t = $('#img_large img');t.prop('src',t.attr('data-original'));$('#img_large').modal();"><img class="cimage " style="clear:left;float:left;margin-right:3px;padding:2px;border:1px solid #ccc;-moz-border-radius:2px;-webkit-border-radius:2px;" src="//ffcdn2012t-fictionpressllc.netdna-ssl.com/image/3368187/75/" width="75" height="100"></span><button class="btn pull-right icon-heart" type="button" onclick='$("#follow_area").modal();'> Follow/Fav</button><b class="xcontrast_txt">Time Turned Back</b>
+<span class="xcontrast_txt"><div style="height:5px"></div>By:</span> <a class="xcontrast_txt" href="/u/6892119/TaraSoleil">TaraSoleil</a> <span class="icon-mail-1  xcontrast_txt"></span> <a class="xcontrast_txt" title="Send Private Message" href="https://www.fanfiction.net/pm2/post.php?uid=6892119"></a>
+<div style="margin-top:2px" class="xcontrast_txt">Broken &amp; angry after losing Sirius fifth year, Harry recklessly puts himself in harms way, dragging Hermione along for the ride. Now they are stuck in another time with very familiar faces. Will the time spent with lost loved ones help heal Harry or only damage him further? HP/SB with a touch of HG/RL to relieve the overbearing angst.</div>
+<span class="xgray xcontrast_txt">Rated: <a class="xcontrast_txt" href="https://www.fictionratings.com/" target="rating">Fiction  T</a> - English - Drama/Angst -  Harry P., Sirius B. - Chapters: 72   - Words: 187,956 - Reviews: <a href="/r/11379661/">449</a> - Favs: 314 - Follows: 516 - Updated: <span data-xutime="1481249615">16h</span> - Published: <span data-xutime="1436878429">7/14/2015</span> - id: 11379661 </span>
+</div>
+        ''',
+
+        dict(
+            title='Time Turned Back',
+            user_id=6892119,
+            username='TaraSoleil',
+            summary='Broken & angry after losing Sirius fifth year, Harry recklessly puts himself in harms way, dragging Hermione along for the ride. Now they are stuck in another time with very familiar faces. Will the time spent with lost loved ones help heal Harry or only damage him further? HP/SB with a touch of HG/RL to relieve the overbearing angst.',
+            follows=516,
+            favorites=314,
+            rating='Fiction T',
+            language='English',
+            genres='Drama/Angst',
+            characters='Harry P., Sirius B.',
+        )
+
+    )
+
 ])
 def test_parse(html, fields):
 
-    html_row = MetadataHTML(book_id=1, html=html)
+    html_row = MetadataHTML(html=html)
 
     row = html_row.parse()
 
