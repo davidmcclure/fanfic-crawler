1b74cc69406756a4c02e896dd1b27dd6262dceeb
David McClure <davidwilliammcclure@gmail.com>
2016-12-11 19:41:28 -0800

    Finish review tests.

--- test/fanfic/models/review_html/test_ingest.py
+++ test/fanfic/models/review_html/test_ingest.py
@@ -40,6 +40,38 @@ def ingest(db_module):
         published=dt.fromtimestamp(1481506897),
     )),
 
+    (244872071, dict(
+        review_id=244872071,
+        book_id=11403360,
+        user_id=5354915,
+        username='Mawenn35',
+        chapter_number=14,
+        review="Please update soon ! I want to see how Minerva will react !",
+        published=dt.fromtimestamp(1481500327),
+    )),
+
+    # Guest user with default "Guest" name.
+    (244833729, dict(
+        review_id=244833729,
+        book_id=11403360,
+        user_id=None,
+        username='Guest',
+        chapter_number=13,
+        review="I love minerva mcgonagall awesome i can't wait more",
+        published=dt.fromtimestamp(1481408736),
+    )),
+
+    # Guest user with custom name.
+    (244481472, dict(
+        review_id=244481472,
+        book_id=11403360,
+        user_id=None,
+        username='dsky',
+        chapter_number=13,
+        review="This is great. Your OC\"s are very interesting, and I like the trips through the mablomi as a way for Harry and Hermione to figure things out. I'm a sucker for MM/AD too. It will be awesome if Albus is alive. I hope he comes back in time to help them fight whoever it was that gave Umbridge the knife. I'm looking forward to more!",  # noqa: E501
+        published=dt.fromtimestamp(1480485582),
+    )),
+
 ])
 def test_ingest(review_id, fields):
 
