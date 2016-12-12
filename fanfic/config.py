4a33199b97b51856dee6b6fc406529fcc6ad85fa
David McClure <davidwilliammcclure@gmail.com>
2016-12-12 10:38:57 -0800

    Switch to Postgres.

--- fanfic/config.py
+++ fanfic/config.py
@@ -55,7 +55,7 @@ class Config(dict):
         """
 
         return URL(**dict(
-            drivername='sqlite',
+            drivername='postgresql',
             database=self['database'],
         ))
 
