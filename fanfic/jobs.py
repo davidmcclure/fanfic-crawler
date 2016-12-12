2ca8ec1b62797e783097cf8e7a0fbf806d40ea84
David McClure <davidwilliammcclure@gmail.com>
2016-12-11 21:58:04 -0800

    Add CLI routes to queue jobs.

--- fanfic/jobs.py
+++ fanfic/jobs.py
@@ -23,3 +23,13 @@ def crawl(spider: str, book_id: int):
 @job('metadata', connection=redis, timeout=3600)
 def crawl_metadata(book_id: int):
     crawl('metadata', book_id)
+
+
+@job('chapters', connection=redis, timeout=3600)
+def crawl_chapters(book_id: int):
+    crawl('chapter', book_id)
+
+
+@job('reviews', connection=redis, timeout=3600)
+def crawl_reviews(book_id: int):
+    crawl('review', book_id)
